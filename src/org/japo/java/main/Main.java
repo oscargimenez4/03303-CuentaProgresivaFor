b942a057be8ed99f2bbd567a81e6757c7094d07a
oscargimenez4 <oscargimenez4@gmail.com>
2019-10-30 10:06:52 +0100

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright 2019 Oscar G.4 - oscargimenez4@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Oscar G.4 - oscargimenez4@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //Constantes
+        final int CUENTA_INI = 0;
+        final int CUENTA_FINAL = 10;
+        //Variables
+        for (int i = CUENTA_INI; i < CUENTA_FINAL; i++) {
+            //Mensaje
+            System.out.println(i);
+        }
+//        int numero = 0;
+//        for (int i = 0; i <= 9; i++) {
+//            System.out.println(numero);
+//            numero++;
+//        }
+    }
+}
